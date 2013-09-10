204c732f40cacc95b103a9219b120fead8cb2ab9
r.takahashi <rtakahashi@takahashi-pc.lan>
2013-09-10 23:44:52 +0900

    created db_registration view modified table

--- testProject/CodeIgniter_Bootstrap/application/controllers/frontpage.php
+++ testProject/CodeIgniter_Bootstrap/application/controllers/frontpage.php
@@ -1,11 +1,19 @@
 <?php if (!defined('BASEPATH')) die();
 class Frontpage extends Main_Controller {
+	
+	function __construct(){
+		parent::__construct();
+	}
 
    public function index()
 	{
       $this->load->view('include/header');
       $this->load->view('frontpage');
       $this->load->view('include/footer');
+
+	  $data['user_id']	= $this->tank_auth->get_user_id();
+	  $data['username']	= $this->tank_auth->get_username();
+	  $this->load->view('welcome', $data);
 	}
    
 }
--- testProject/CodeIgniter_Bootstrap/application/core/Main_Controller.php
+++ testProject/CodeIgniter_Bootstrap/application/core/Main_Controller.php
@@ -4,5 +4,15 @@ class Main_Controller extends MY_Controller
    function __construct()
    {
       parent::__construct();
+      
+      $this->load->helper('url');
+      $this->load->library('tank_auth');
+      
+      if (!$this->tank_auth->is_logged_in()) {
+   		echo current_url();
+     	if (!strstr(current_url(),'/auth/')){
+     		redirect('/auth/login/');
+     	}
+      }
    }
 }
